0d898d552386e330739f33015e5f9e5292337f0d
U-FTT\tomera <tomera@Tomera1-hrdt.fundtech.isr>
2018-02-21 14:57:12 +0000

    redirecting with node

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var http 	= require('http');
 var path 	= require('path');			//work with paths
-
+var request = require('request');
+var morgan = require('morgan')
 var app = express();  
+const bodyParser = require('body-parser');
+const snsSubscriptionConfirmation = require('aws-sns-subscription-confirmation');
+
 
 const PORT = process.env.PORT || 5000;
+
+
 app.set('port', PORT);
 
 //app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
@@ -12,11 +18,26 @@ app.set('port', PORT);
 
 
 app.use(express.static(__dirname + '/public')); 
+app.use(morgan('combined'));
+app.use(snsSubscriptionConfirmation.overrideContentType());
+app.use(bodyParser.json());
+app.use(snsSubscriptionConfirmation.snsConfirmHandler());
+
+app.use('/iotbutton', function(req, res){
+	
+	req.pipe(request('http://iotpay.herokuapp.com/contract/iotbutton')).pipe(res);
+//	request.get("http://iotpay.herokuapp.com/contract/iotbutton").on('response',function(res){
+//		console.log(res.statusCode); // 200
+  //  	console.log(res.headers['content-type']);
+//	})
+});
 
 //routes
 app.use('/*', function(req, res){	    
   res.sendFile('./public/index.html');
 });
+
+
 
 http
 	.createServer(app)
